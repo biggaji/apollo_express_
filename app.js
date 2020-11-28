21518c0aac1119f48e35ed9542dead88fd7ed3e8
Tobi Ajibade <herityjohnny14>
2020-11-28 13:36:12 -0800

    Configured apollo server for express

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
 
 const app = express();
 
+
+//fake schema
+const typeDefs = gql`
+    type Book {
+        title: String
+    }
+
+    type Query {
+        books: Book
+    }
+`;
+
+// Fake resolver
+const resolvers = {
+    Query: {
+        books: () => "Hello world"
+    }
+}
+
+
 const PORT = process.env.PORT || 5000;
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.applyMiddleware({ app });
+// Can now access graphql through /graphql
 
 app.listen(PORT, () => { console.log("Server running --- full speed at ", PORT) })
